88cbc55236c3bd3929a4dbae07a422d5cf8d7bf2
HaraldWik <wikhallharald@gmail.com>
2025-08-30 12:21:02 +0200

    Small fixes

--- build.zig
+++ build.zig
@@ -4,46 +4,37 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // const sdl = b.dependency("sdl", .{
-    //     .target = target,
-    //     .optimize = optimize,
-    //     .lto = optimize != .Debug,
-    //     .preferred_linkage = .dynamic,
-    // });
+    const sdl = b.dependency("sdl", .{
+        .target = target,
+        .optimize = optimize,
+        .lto = optimize != .Debug,
+        .preferred_linkage = .dynamic,
+    });
 
-    // const sdl_translate_c = b.addTranslateC(.{
-    //     .root_source_file = sdl.path("include/SDL3/SDL.h"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    // sdl_translate_c.addIncludePath(sdl.path("include"));
-    // const c_mod = sdl_translate_c.createModule();
-    // c_mod.addIncludePath(sdl.path("include/"));
+    const sdl_c = b.addTranslateC(.{
+        .root_source_file = sdl.path("include/SDL3/SDL.h"),
+        .target = target,
+        .optimize = optimize,
+    });
+    sdl_c.addIncludePath(sdl.path("include"));
 
-    const c_mod = b.addModule("c", .{
-        .root_source_file = b.addWriteFiles().add("c.zig",
-            \\pub const c = @cImport({
-            \\  @cInclude("SDL3/SDL.h");
-            \\  @cInclude("SDL3/SDL_vulkan.h");
-            \\});
-        ),
+    const vulkan = b.addTranslateC(.{
+        .root_source_file = b.dependency("vulkan_headers", .{}).path("include/vulkan/vulkan.h"),
         .target = target,
         .optimize = optimize,
         .link_libc = true,
-    });
-    c_mod.linkSystemLibrary("SDL3", .{});
-
-    const wgpu_mod = b.dependency("wgpu_native_zig", .{}).module("wgpu");
+    }).createModule();
+    vulkan.linkSystemLibrary("vulkan", .{});
 
     const mod = b.addModule("engine", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .imports = &.{
-            .{ .name = "wgpu", .module = wgpu_mod },
-            .{ .name = "c", .module = c_mod },
+            .{ .name = "sdl", .module = sdl_c.createModule() },
+            .{ .name = "vulkan", .module = vulkan },
         },
     });
-    mod.linkSystemLibrary("vulkan", .{});
+    mod.linkSystemLibrary("SDL3", .{});
 
     const exe = b.addExecutable(.{
         .name = "engine",
@@ -53,7 +44,6 @@ pub fn build(b: *std.Build) void {
             .optimize = optimize,
             .imports = &.{
                 .{ .name = "engine", .module = mod },
-                .{ .name = "c", .module = c_mod },
             },
         }),
     });
@@ -61,28 +51,19 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(exe);
 
     const run_step = b.step("run", "Run the app");
-
     const run_cmd = b.addRunArtifact(exe);
     run_step.dependOn(&run_cmd.step);
-
     run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    if (b.args) |args| run_cmd.addArgs(args);
 
     const mod_tests = b.addTest(.{
         .root_module = mod,
     });
-
     const run_mod_tests = b.addRunArtifact(mod_tests);
-
     const exe_tests = b.addTest(.{
         .root_module = exe.root_module,
     });
-
     const run_exe_tests = b.addRunArtifact(exe_tests);
-
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_tests.step);
--- src/window.zig
+++ src/window.zig
@@ -1,20 +1,23 @@
 const std = @import("std");
-const c = @import("c").c;
+const sdl = @import("sdl");
+const vk = @import("vulkan");
+
+extern "SDL3" fn SDL_Vulkan_CreateSurface(window: ?*sdl.SDL_Window, instance: vk.VkInstance, allocator: ?*const vk.VkAllocationCallbacks, surface: *vk.VkSurfaceKHR) callconv(.c) bool;
 
 pub const Window = opaque {
-    pub inline fn toC(self: *@This()) ?*c.SDL_Window {
+    pub inline fn toC(self: *@This()) ?*sdl.SDL_Window {
         return @ptrCast(@alignCast(self));
     }
 
     pub fn init(title: [*:0]const u8, width: usize, height: usize) !*@This() {
-        if (!c.SDL_Init(c.SDL_INIT_VIDEO)) {
-            std.log.err("Failed init SDL3: {s}", .{c.SDL_GetError()});
+        if (!sdl.SDL_Init(sdl.SDL_INIT_VIDEO)) {
+            std.log.err("Failed init SDL3: {s}", .{sdl.SDL_GetError()});
             return error.SdlInit;
         }
 
-        const window = c.SDL_CreateWindow(title, @intCast(width), @intCast(height), c.SDL_WINDOW_RESIZABLE | c.SDL_WINDOW_VULKAN) orelse {
-            std.log.err("Failed to create window: {s}", .{c.SDL_GetError()});
-            c.SDL_Quit();
+        const window = sdl.SDL_CreateWindow(title, @intCast(width), @intCast(height), sdl.SDL_WINDOW_RESIZABLE | sdl.SDL_WINDOW_VULKAN) orelse {
+            std.log.err("Failed to create window: {s}", .{sdl.SDL_GetError()});
+            sdl.SDL_Quit();
             return error.SdlCreateWindow;
         };
 
@@ -22,15 +25,15 @@ pub const Window = opaque {
     }
 
     pub fn deinit(self: *@This()) void {
-        c.SDL_DestroyWindow(self.toC());
-        c.SDL_Quit();
+        sdl.SDL_DestroyWindow(self.toC());
+        sdl.SDL_Quit();
     }
 
     pub fn shouldClose(_: *@This()) bool {
-        var event: c.SDL_Event = undefined;
-        while (c.SDL_PollEvent(&event)) {
+        var event: sdl.SDL_Event = undefined;
+        while (sdl.SDL_PollEvent(&event)) {
             switch (event.type) {
-                c.SDL_EVENT_QUIT, c.SDL_EVENT_TERMINATING => return true,
+                sdl.SDL_EVENT_QUIT, sdl.SDL_EVENT_TERMINATING => return true,
                 else => break,
             }
         }
@@ -39,8 +42,8 @@ pub const Window = opaque {
 
     pub fn getSize(self: *@This()) !struct { usize, usize } {
         var size: struct { c_int, c_int } = undefined;
-        if (!c.SDL_GetWindowSize(self.toC(), &size.@"0", &size.@"1")) {
-            std.log.err("Failed to get window size: {s}", .{c.SDL_GetError()});
+        if (!sdl.SDL_GetWindowSize(self.toC(), &size.@"0", &size.@"1")) {
+            std.log.err("Failed to get window size: {s}", .{sdl.SDL_GetError()});
             return error.SdlGetWindowSize;
         }
         return .{ @intCast(size.@"0"), @intCast(size.@"1") };
@@ -48,16 +51,16 @@ pub const Window = opaque {
 
     pub fn getVulkanInstanceExtensions(_: *@This()) [][*:0]const u8 {
         var count: u32 = 0;
-        var extensions = c.SDL_Vulkan_GetInstanceExtensions(&count);
-        return @constCast(@ptrCast(extensions[0..count]));
+        var extensions = sdl.SDL_Vulkan_GetInstanceExtensions(&count);
+        return @ptrCast(@constCast(extensions[0..count]));
     }
 
     pub fn initVulkanSurface(self: *anyopaque, instance: *anyopaque) !*anyopaque {
-        var surface: c.VkSurfaceKHR = undefined;
-        if (!c.SDL_Vulkan_CreateSurface(@ptrCast(self), @ptrCast(instance), null, &surface)) {
-            std.log.err("Failed to create vulkan surface in SDL3: {s}", .{c.SDL_GetError()});
+        var surface: ?*anyopaque = undefined;
+        if (!SDL_Vulkan_CreateSurface(@ptrCast(self), @ptrCast(instance), null, &surface)) {
+            std.log.err("Failed to create vulkan surface in SDL3: {s}", .{sdl.SDL_GetError()});
             return error.SdlVulkanCreateSurface;
         }
-        return @ptrCast(surface);
+        return surface orelse return error.SdlVulkanCreateSurface;
     }
 };
